f8a146b8378e99601b2481fc8e8d07c7ca8e9a16
Jonathan Minton <nate.minton@gmail.com>
2014-06-26 09:19:02 +0100

    Add tile plots

--- Script.R
+++ Script.R
@@ -60,61 +60,164 @@ eu_res_total <- Outlist$residuals$total[europe_indicators]
 #  country, gender, age, year
 # value: quantity
 
-fm <- function(x){
-  out <- melt(x, varnames=c("age", "year"), value.name="residual")
-  out <- data.frame(out, sex="male")
+fn <- function(x, label=NA, sex_=NA){
+  out <- melt(x, varnames=c("age", "year"), value.name=label)
+  out <- data.frame(out, sex=sex_)
   return(out)            
 }
 
-ff <- function(x){
-  out <- melt(x, varnames=c("age", "year"), value.name="residual")
-  out <- data.frame(out, sex="female")
-  return(out)            
-}
-
-ft <- function(x){
-  out <- melt(x, varnames=c("age", "year"), value.name="residual")
-  out <- data.frame(out, sex="total")
-  return(out)            
-}
 
 dta_long_male <- ldply(
   eu_res_male,
-  fm
+  fn,
+  label="residual",
+  sex_="male"
   )
-
-names(dta_long_male)[1] <- "country"
 
 dta_long_female <- ldply(
   eu_res_female,
-  ff
+  fn, 
+  label="residual",
+  sex_="female"
   )
-
-names(dta_long_female)[1] <- "country"
 
 dta_long_total <- ldply(
   eu_res_total,
-  ft
+  fn,
+  label="residual",
+  sex_="total"
   )
 
+names(dta_long_male)[1] <- "country"
+names(dta_long_female)[1] <- "country"
+names(dta_long_total)[1] <- "country"
+
+
+dta_long <- rbind(dta_long_male, dta_long_female, dta_long_total)
+
+rm(dta_long_male, dta_long_female, dta_long_total)
+
+dta_long2 <- melt(dta_long, id.var=c("country", "age", "year", "sex"))
+dta_wide <- dcast(dta_long2, country + age + year ~ sex)
+
+
+dta_res_tidy <- dta_long2
+
+
+####################################################################################
+
+eu_exp_male <- Outlist$expectations$male[europe_indicators]
+eu_exp_female <- Outlist$expectations$female[europe_indicators]
+eu_exp_total <- Outlist$expectations$total[europe_indicators]
+
+dta_long_male <- ldply(
+  eu_exp_male,
+  fn,
+  label="expectation",
+  sex_="male"
+)
+
+dta_long_female <- ldply(
+  eu_exp_female,
+  fn, 
+  label="expectation",
+  sex_="female"
+)
+
+dta_long_total <- ldply(
+  eu_exp_total,
+  fn,
+  label="expectation",
+  sex_="total"
+)
+
+
+names(dta_long_male)[1] <- "country"
+names(dta_long_female)[1] <- "country"
 names(dta_long_total)[1] <- "country"
 
 dta_long <- rbind(dta_long_male, dta_long_female, dta_long_total)
 
 rm(dta_long_male, dta_long_female, dta_long_total)
 
-dta_long2 <- melt(dta_long, id.var=c(1,2,3,5))
-dta_wide <- dcast(dta_long2, country + age + year ~ sex)
+dta_long2 <- melt(dta_long, id.var=c("country", "age", "year", "sex"))
 
+dta_tidy_long <- rbind(dta_res_tidy, dta_long2)
+
+dta_tidy <- dcast(dta_tidy_long, country + age + year + sex ~ variable)
 # g <- ggplot(subset(dta_wide, year > 1995)) + aes(x=year, y=male, group=age)
+dta_tidy <- mutate(dta_tidy, residual_proportion=residual/expectation)
 
-# dta_wide2 <- data.frame(dta_long2, age_group=cut_interval(dta_wide$age, 10))
+save(dta_tidy, file="Data/RObj/Tidy_Data.RData")
 
 
-g <- ggplot(subset(dta_long2, year > 1995)) + aes(x=year, y=value, group=age, colour=age)
-g + geom_line() + facet_grid(country ~ sex) + coord_flip()
+####################
+## contour plot
+load("Data/RObj/Tidy_Data.RData")
+
+g <- ggplot(subset(dta_tidy, year > 2000 & sex=="male")) + aes(x=year, y=age, z=residual_proportion)
+g2 <- g + geom_tile(aes(fill=residual_proportion)) + facet_wrap ( ~ country, nrow=4)
+g3 <- g2 + scale_fill_gradientn(
+  colours=c("blue", "white", "red"), limits=c(-0.15, 0.15)
+  )
+print (g3)
+
+ggsave("Figures/Tile_Male.png")
+
+#################
+g <- ggplot(subset(dta_tidy, year > 2000 & sex=="female")) + aes(x=year, y=age, z=residual_proportion)
+g2 <- g + geom_tile(aes(fill=residual_proportion)) + facet_wrap ( ~ country, nrow=4)
+g3 <- g2 + scale_fill_gradientn(
+  colours=c("blue", "white", "red"), limits=c(-0.15, 0.15)
+)
+print (g3)
+
+ggsave("Figures/Tile_Female.png")
+
+########
+g <- ggplot(subset(dta_tidy, year > 2000 & sex=="total")) + aes(x=year, y=age, z=residual_proportion)
+g2 <- g + geom_tile(aes(fill=residual_proportion)) + facet_wrap ( ~ country, nrow=4)
+g3 <- g2 + scale_fill_gradientn(
+  colours=c("blue", "white", "red"), limits=c(-0.15, 0.15)
+)
+print (g3)
+
+ggsave("Figures/Tile_Total.png")
+
+
+g <- ggplot(subset(dta_tidy, year > 2000)) + aes(x=year, y=residual_proportion, group=age, colour=age)
+g2 <- g + geom_line() + facet_grid(country ~ sex)
+print(g2)
 
 ggsave("Figures/All_Lattice.png")
+
+
+
+
+# # log on y axis
+# 
+# ?coord_cartesian
+# 
+# g3 <- g2 + scale_y_log10()
+
+# Separate by gender?
+g <- ggplot(subset(dta_tidy, year > 2000 & sex=="male")) + aes(x=year, y= residual_proportion, group=age, colour=age)
+g2 <- g + geom_line() + facet_wrap( ~ country, nrow=4)
+print(g2)
+ggsave("Figures/Lattice_Male.png")
+
+
+g <- ggplot(subset(dta_tidy, year > 2000 & sex=="female")) + aes(x=year, y= residual_proportion, group=age, colour=age)
+g2 <- g + geom_line() + facet_wrap( ~ country, nrow=4)
+print(g2)
+
+ggsave("Figures/Lattice_Female.png")
+
+# Log scale on y axis
+
+#### What I now want are the residuals as a percentage of the expectation
+
+
 
 
 ### What I want: 
