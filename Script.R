72ab636b52defd8d032da4b14dce47df612b1b69
Jonathan Minton <nate.minton@gmail.com>
2014-06-30 18:43:01 +0100

    Additional subgroup: 14 countries observed in 2011

--- Script.R
+++ Script.R
@@ -246,8 +246,16 @@ print(g4)
 ggsave("Figures/lines_residual_deathrates.png")
 
 
-# now I want something that adds points for each country to 
-# g3 above
+###############################################################################################
+###############################################################################################
+
+## as above but only the 14 countries in 2011
+europe_codes <- country_codes$short[which(country_codes$europe==1)]
+
+tmp <- dcast(counts, year + country ~ . , length) 
+tmp <- subset(tmp, subset=year==2011)
+tmp[,3] <- NULL
+eu_2011_countries <- as.character(tmp$country[which(tmp$country %in%  europe_codes)])
 
 
 ####################################################################################
@@ -287,3 +295,172 @@ print(g3)
 ggsave("Figures/Tile_Countries_Combined_from1950.png")
 ####################
 
+
+# As above, but only the 14 countries included in 2011
+# join exp_eu_all and rates_eu_all
+
+mrate_resp <- join(
+  rates_14_all,
+  exp_14_all, 
+  by=c("year", "age", "sex"),
+  type="inner"
+)
+
+
+mrate_resp <- arrange(mrate_resp, age, year, sex)
+
+
+mrate_resp$age_group <- recode(
+  mrate_resp$age,
+  recodes="
+  1:4='4 or under';
+  5:9 = '5-9';
+  10:14 = '10-14';
+  15:19 = '15-19';
+  20:24 = '20-24';
+  25:29 = '25-29';
+  30:34 = '30-34';
+  35:39 = '35-39';
+  40:44 = '40-44';
+  45:49 = '45-49';
+  else = '50 or older'
+  ",
+  as.factor.result=T,
+  levels=c(
+    '4 or under',
+    '5-9',
+    '10-14',
+    '15-19',
+    '20-24',
+    '25-29',
+    '30-34',
+    '35-39',
+    '40-44',
+    '45-49',
+    '50 or older'
+  )  
+)
+
+# cut ages into groups 
+
+mrate_resp$age_group <- ordered(
+  mrate_resp$age_group, 
+  levels=c(
+    '4 or under',
+    '5-9',
+    '10-14',
+    '15-19',
+    '20-24',
+    '25-29',
+    '30-34',
+    '35-39',
+    '40-44',
+    '45-49',
+    '50 or older'
+  )
+)
+
+
+
+g <- ggplot(subset(
+  mrate_resp,
+  subset= sex!="total" & age <=50 & age > 20 & year > 1990
+))
+
+g2 <- g + aes(x=residual_prop, y=log(death_rate), group=sex)
+g3 <- g2 + geom_line(aes(colour=year)) + facet_wrap( ~ age) + geom_point(aes(pch=sex), alpha=0.2)
+g4 <- g3 + scale_colour_gradient(low="blue", high="red")
+g4
+
+ggsave("Figures/deathrate_vs_res_facet_age.png")
+
+
+####
+g <- ggplot(
+  subset(
+    mrate_resp,
+    subset= sex!="total" & age <= 50 & age >=20 & year >=1990
+  )
+)
+
+g2 <- g + aes(x=year, y=log(death_rate), group=age, colour=age)
+g3 <- g2 + geom_line() + facet_wrap(~ sex) 
+print(g3)
+
+ggsave("Figures/deathrates_age.png")
+
+
+mrate_resp_agegroup <- ddply(
+  mrate_resp, 
+  .(sex, year, age_group), 
+  summarise,
+  population_count=sum(population_count),
+  death_count=sum(death_count),
+  population_actual=sum(population_actual),
+  population_expected=sum(population_expected),
+  death_rate=death_count/population_count,
+  residual_count=population_actual - population_expected,
+  residual_prop=residual_count/population_actual
+)
+
+mrate_resp_agegroup <- arrange(mrate_resp_agegroup, age_group, sex, year)
+####
+
+agegroups_of_interest <- c(
+  '20-24',
+  '25-29',
+  '30-34',
+  '35-39',
+  '40-44',
+  '45-49'
+)
+
+
+
+g <- ggplot(
+  subset(
+    mrate_resp_agegroup,
+    subset= sex!="total" & year >=1990 & age_group %in% agegroups_of_interest
+  )
+)
+
+g2 <- g + aes(x=year, y=log(death_rate), group=age_group, colour=age_group, lty=age_group)
+g3 <- g2 + geom_line(size=1.1) + facet_wrap(~ sex) 
+print(g3)
+
+ggsave("Figures/deathrates_agegroup_2011countriesonly.png")
+
+
+
+#############################################################################
+
+g <- ggplot(
+  subset(
+    mrate_resp_agegroup,
+    subset= sex!="total" & year >=1990 & age_group %in% agegroups_of_interest
+  )
+)
+
+g2 <- g + aes(x=residual_prop, y=log(death_rate), group=age_group, colour=year)
+g3 <- g2 + geom_line(size=1.1) + facet_grid(age_group~ sex) + geom_vline(x=0, lty="dashed") 
+print(g3)
+
+ggsave("Figures/deathrates_resprop_agegroup.png")
+
+g3 <- g2 + geom_point() + facet_grid(age_group~ sex) + geom_vline(x=0, lty="dashed") 
+print(g3)
+ggsave("Figures/deathrates_resprop_agegroup_scatter.png")
+
+
+g <- ggplot(
+  subset(mrate_resp_agegroup,
+         subset= sex!="total" & year >=1990 &  year <= 2011 & age_group %in% agegroups_of_interest))
+
+g2 <- g + aes(y=residual_prop, x=log(death_rate), group = sex, colour=year)
+g3 <- g2 + geom_path(size=1.1) + facet_grid(age_group~.) + geom_hline(y=0, lty="dashed")
+g4 <- g3 + geom_point(colour = "black", shape= "|", size = 3)
+
+print(g4)
+
+ggsave("Figures/deathrates_residual_majaplot_2011countriesonly.png")
+########################################################################
--- Scripts/arrange_tidy_data.r
+++ Scripts/arrange_tidy_data.r
@@ -41,7 +41,7 @@ exp_eu_all <- ddply(
   summarise,
   population_actual=sum(population_actual),
   population_expected=sum(population_expected),
-  residual_count=population_expected - population_actual,
+  residual_count=population_actual - population_expected,
   residual_prop=residual_count/population_actual
 )
 
@@ -52,7 +52,7 @@ write.csv(exp_eu_all, file="Data/Tidy/exp_eu_all.csv", row.names=F)
 
 residuals <- mutate(
   expectations, 
-  population_residual =population_expected - population_actual, 
+  population_residual =population_actual - population_expected, 
   residual_prop=population_residual/population_actual
 )
 
@@ -66,19 +66,78 @@ dta_joined <- subset(dta_joined, subset=europe==1)
 dta_joined$europe <- NULL
 
 
-# 
-# 
-# 
-# country_codes <- read.csv("Data/HMD/country_codes__new.csv", stringsAsFactors=FALSE)
-# 
-# europe_indicators <- country_codes$short[country_codes$europe==T] 
-# 
-# counts_europe <- subset(counts_tidy, subset=country %in% europe_indicators)
-# 
-# counts_europe_aggregated <- ddply(counts_europe, .(year, age, sex), 
-#                                   summarise, 
-#                                   death_count=sum(death_count),
-#                                   population_count=sum(population_count),
-#                                   death_rate =death_count / population_count
-# )
-# 
+
+##### Want to do the above but only for the 14 countries observed in 2011
+countries_2011 <- c(
+  "BEL", "CZE", "DEUTNP", "DNK", "ESP", "EST",
+  "FRATNP", "GBR_NIR", "GBR_SCO", "GBRTENW",
+  "LTU",     "LVA",     "PRT",     "SWE"
+)
+
+
+counts_14 <- subset(
+  counts,
+  subset=country %in% countries_2011                  
+)
+
+write.csv(counts_14, file="Data/Tidy/counts_14.csv", row.names=F)
+
+rates_14 <- subset(
+  rates,
+  subset=country %in% countries_2011
+)
+write.csv(rates_14, file="Data/Tidy/rates_14.csv", row.names=F)
+
+exp_14 <- subset(
+  expectations,
+  subset=country %in% countries_2011
+)
+write.csv(exp_14, file="Data/Tidy/exp_14.csv", row.names=F)
+
+
+counts_14_all <- ddply(
+  counts_14,
+  .(sex, year, age),
+  summarise,
+  n_countries=length(death_count),
+  death_count=sum(death_count),
+  population_count=sum(population_count)
+)
+
+write.csv(counts_14_all, file="Data/Tidy/counts_14_all.csv", row.names=F)
+
+rates_14_all <- mutate(counts_14_all, death_rate=death_count/population_count)
+
+write.csv(rates_14_all, file="Data/Tidy/rates_14_all.csv", row.names=F)
+
+exp_14_all <- ddply(
+  exp_14,
+  .(sex, year, age),
+  summarise,
+  population_actual=sum(population_actual),
+  population_expected=sum(population_expected),
+  residual_count=population_actual - population_expected,
+  residual_prop=residual_count/population_actual
+)
+
+write.csv(exp_eu_all, file="Data/Tidy/exp_14_all.csv", row.names=F)
+
+
+
+
+residuals <- mutate(
+  expectations, 
+  population_residual =population_actual - population_expected, 
+  residual_prop=population_residual/population_actual
+)
+
+residuals$population_actual <- NULL
+residuals$population_expected <- NULL
+residuals$population_residual <- NULL
+
+dta_joined <- join(residuals, rates, by=c("country", "year", "age", "sex"), type="inner")
+dta_joined <- merge(dta_joined, country_codes, by.x="country", by.y="short")
+dta_joined <- subset(dta_joined, subset=europe==1)
+dta_joined$europe <- NULL
+
+
